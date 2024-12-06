5e7e3009090465a91035a31f63611c64254e7b34
Tharush-Somashekar <tharush.somashekar@gmail.com>
2024-12-06 14:26:59 +0530

    herosection writting

--- src/Pages/Home/Herosection.jsx
+++ src/Pages/Home/Herosection.jsx
@@ -1,4 +1,22 @@
 export default function Herosection () {
-    return  <h1>Hero</h1>
+    return(
+        <section id="herosection" className="hero-section">
+            <div className="hero-section-content-container">
+                <div className="hero--section--content">
+                    <h1>
+                        <p assName="section--title">Hey! I am tharush Somashekar</p>
+                        <span className="hero--section-title--color">User Experience</span>{" "}
+                    <br />
+                    Designer
+                    </h1>
+                    <p className="hero--section-description">
+                        Change this in the Herosection.jsx file
+                    <br />
+                    this too dude
+                    </p>
+                </div>    
+            </div>
+        </section>
+    )
     
 }
