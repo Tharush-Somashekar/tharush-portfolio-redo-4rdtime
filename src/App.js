f407b79458d996f760321239fb5b52ecc4633f38
Tharush-Somashekar <tharush.somashekar@gmail.com>
2024-12-06 18:36:03 +0530

    linking import pages

--- src/App.js
+++ src/App.js
@@ -3,6 +3,11 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes} from "react-router-dom";
 import Home from './Pages/Home/Homescreen/Index.jsx';
 import Navbar from './Pages/Home/Navbar.jsx';
+import MySkills from './Pages/Home/Skills.jsx';
+import AboutMe from './Pages/Home/Aboutme.jsx';
+import MyPortfolio from './Pages/Home/Myportfolio.jsx';
+import ContactMe from './Pages/Home/Contactme.jsx';
+import Footer from './Pages/Home/Footer.jsx'
 
 function App() {
   return (
