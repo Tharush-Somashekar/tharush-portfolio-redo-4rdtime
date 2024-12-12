2bd7336ff497b63f75bb336bff486165dbb90b7b
Tharush-Somashekar <tharush.somashekar@gmail.com>
2024-12-12 17:56:16 +0530

    adding services section

--- src/App.js
+++ src/App.js
@@ -16,6 +16,7 @@ function App() {
         <Navbar />
         <Home />
         <AboutMe/>
+        <MySkills/>
     </div>
   );
 }
--- src/Pages/Home/Skills.jsx
+++ src/Pages/Home/Skills.jsx
@@ -4,7 +4,7 @@ export default function MySkills() {
   return (
     <section className="skills--section" id="mySkills">
       <div className="portfolio--container">
-        <p className="section--title">Skills</p>
+        <p className="section--title"></p>
         <h2 className="skills--section--heading">My Services</h2>
       </div>
       <div className="skills--section--container">
