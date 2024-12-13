cd44df99142d8b1c28d17f2c6ed1e20fd6537c8e
Tharush-Somashekar <tharush.somashekar@gmail.com>
2024-12-13 07:34:55 +0530

    Project content addition + linking testimonials

--- src/Pages/Home/Homescreen/Index.jsx
+++ src/Pages/Home/Homescreen/Index.jsx
@@ -3,6 +3,7 @@ import Aboutme from "../Aboutme";
 import Skills from "../Skills";
 import MyPortfolio from "../Myportfolio";
 import Footer from "../Footer";
+import Testimonial from "../Testimonials";
 
 export default function Home() {
     return (
@@ -11,6 +12,7 @@ export default function Home() {
         <Aboutme/>
         <Skills/>
         <MyPortfolio/>
+        <Testimonial/>
         <Footer/>
       </>
     );
